4d4419bd66fae56b98f98e642510ddb453496a7d
MaquinoBSC <martinare.ipn@gmail.com>
2022-06-06 13:17:50 -0500

    Comunicación entre servidor y cliente para agregar un siguiente ticket

--- public/js/nuevo-ticket.js
+++ public/js/nuevo-ticket.js
@@ -1,3 +1,24 @@
+// Referencias HTML
+const lblNuevoTicket= document.querySelector('#lblNuevoTicket');
+const btnCrear= document.querySelector('button');
 
 
-console.log('Nuevo Ticket HTML');
+const socket = io();
+
+socket.on('connect', () => {
+    btnCrear.disabled= false;
+});
+
+socket.on('disconnect', () => {
+    btnCrear.disabled= true;
+});
+
+socket.on('ultimo-ticket', ( payload )=> {
+    lblNuevoTicket.innerText= 'Ultimo ticket: ' + payload;
+});
+
+btnCrear.addEventListener( 'click', () => {
+    socket.emit( 'siguiente-ticket', null, ( ticket ) => {
+        lblNuevoTicket.innerText= ticket;
+    });
+});
--- sockets/controller.js
+++ sockets/controller.js
@@ -3,15 +3,14 @@ const TicketControl = require('../models/ticket-control');
 const ticketControl= new TicketControl();
 
 const socketController = (socket) => {
-    // socket.on('disconnect', () => {
-    //     console.log('Cliente desconectado', socket.id );
-    // });
-    socket.on('enviar-mensaje', ( payload, callback ) => {
-        const id = 123456789;
-        callback( id );
-        socket.broadcast.emit('enviar-mensaje', payload );
+    socket.emit( 'ultimo-ticket', ticketControl.ultimo );
+    
+    socket.on('siguiente-ticket', ( payload, callback ) => {
+        const siguiente= ticketControl.siguiente();
+        callback( siguiente );
     });
 
+    // TODO: Notificar que hay un nuevo ticket sin asignar 
 }
 
 module.exports = {
